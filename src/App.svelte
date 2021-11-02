ad697b238e8a43725d7c0b322440483c30256223
MohamedElmdary <engm5081@gmail.com>
2021-11-02 12:32:16 +0200

    Added sidenav supporting any number of indentation

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
-  import VM from "./elements/vm/Vm.wc.svelte";
+  import Sidenav from "./elements/sidenav/Sidenav.wc.svelte";
+  import yaml from "js-yaml";
+  import type { ISidenavRoute } from "./types";
+
+  let routes: ISidenavRoute[];
+  fetch("/_sidenav.yaml")
+    .then((res) => res.text())
+    .then((res) => yaml.load(res) as ISidenavRoute[])
+    .then((_routes) => (routes = _routes));
 </script>
 
-<VM />
+{#if routes}
+  <Sidenav {routes} />
+{/if}
--- src/elements/sidenav/Sidenav.wc.svelte
+++ src/elements/sidenav/Sidenav.wc.svelte
@@ -1,54 +1,93 @@
 <svelte:options tag={null} />
 
 <script lang="ts">
-  import type { ISidenavRoute, IGlobalOptions } from "../../types";
-  import resolveTheme from "../../utils/resolveTheme";
+  import type { ISidenavRoute } from "../../types";
 
-  export let routes: string;
-  export let theme: IGlobalOptions["theme"];
-
-  let _routes: ISidenavRoute[];
-  let _theme: IGlobalOptions["theme"];
-  $: {
-    _routes = routes ? JSON.parse(routes) : [];
-    _theme = resolveTheme(theme);
-  }
-
-  let active: string = null;
+  export let routes: ISidenavRoute[];
+  export let root: boolean = true;
 
   function onRoute(route: ISidenavRoute) {
-    active = route.to;
     console.log({ route });
   }
+
+  const isLabel = ({ path, to }: ISidenavRoute) => !path && !to;
+  const hasChildren = ({ children: c }: ISidenavRoute) => c && c.length;
 </script>
 
-{#if _theme === "bulma"}
-  <!-- currently supporting only 2 levels -->
-  <aside class="menu">
-    {#each _routes as route}
-      <p class="menu-label">{route.label}</p>
-      {#if route.children?.length}
-        <ul class="menu-list">
-          {#each route.children as child}
-            <li>
-              <a
-                class={active === child.to ? "is-active" : undefined}
-                href={`/#${child.to}`}
-                on:click={onRoute.bind(undefined, child)}
-              >
-                {child.label}
-              </a>
-            </li>
-          {/each}
-        </ul>
-      {/if}
+{#if root}
+  <section class="sidenav">
+    <aside class="menu">
+      {#each routes as route (route.label)}
+        {#if isLabel(route)}
+          <p class="menu-label">
+            {route.label}
+          </p>
+
+          {#if hasChildren(route)}
+            <svelte:self routes={route.children} root={false} />
+          {/if}
+        {/if}
+      {/each}
+    </aside>
+  </section>
+{:else}
+  <ul class="menu-list">
+    {#each routes as route (route.label)}
+      <li>
+        <!-- class={Math.random() > 0.5 ? "is-active" : ""} -->
+        <a
+          href={route.to}
+          on:click|preventDefault={onRoute.bind(undefined, route)}
+        >
+          {route.label}
+        </a>
+
+        {#if hasChildren(route)}
+          <svelte:self routes={route.children} root={false} />
+        {/if}
+      </li>
     {/each}
-  </aside>
+  </ul>
 {/if}
 
 <style scoped lang="scss">
-  @import "bulma/sass/utilities/_all.sass";
-  @import "bulma/sass/base/generic.sass";
-  @import "bulma/sass/base/minireset.sass";
-  @import "bulma/sass/components/menu.sass";
+  // @import "bulma/sass/utilities/_all.sass";
+  // @import "bulma/sass/base/generic.sass";
+  // @import "bulma/sass/base/minireset.sass";
+  // @import "bulma/sass/components/menu.sass";
+  @import url("/assets/bulma.min.css");
+
+  .sidenav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 300px;
+    height: 100vh;
+    padding: 15px;
+    overflow-x: hidden;
+    overflow-y: auto;
+    padding-bottom: 50px;
+    border-right: 1px solid #e8e8e8;
+
+    &::-webkit-scrollbar {
+      width: 10px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background-color: #d3d3d3;
+    }
+
+    &::after {
+      content: "";
+      position: fixed;
+      bottom: 0;
+      height: 50px;
+      width: 100%;
+      left: 0;
+      background-image: linear-gradient(
+        rgba(245, 247, 249, 0),
+        rgb(245, 247, 249)
+      );
+    }
+  }
 </style>
